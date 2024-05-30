f293145650ffb33d92a51a3bafa11c4feada48ed
jaykalathiya123 <kalathiyajay020>
2024-05-30 17:18:52 +0530

    bt-PORTFOLIO

---
+++ assest/js/const swiper.js
@@ -0,0 +1,36 @@
+    const swiper = new Swiper('.swiper', {
+// Optional parameters
+        slidesPerView: 3,
+       autoplay:true,
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+        });
